88262b4b123f00a2a77e7a1030a5bbef40443c76
John DeOrian <jdeorian@gmail.com>
2018-08-03 11:11:38 -0700

    commit

--- myFirstClass.py
+++ myFirstClass.py
@@ -1,69 +1,6 @@
 import random
 import numpy as np
 
-class board1:
-    
-    def __init__(self): # initialize a new board
-        import random
-        self.game_status = "Playing"
-        self.positions = [0,0,0,0]
-        self.add_block()
-        self.add_block()
-        print(self.positions)
-        self.user_action = "n/a"
-        while self.user_action != "quit" and self.game_status != "Game Over":
-            print("Type 'a' to swipe left. Type 'quit' to quit.")
-            self.user_action = input()
-            if self.user_action == "a":
-                self.swiped_left()
-            elif self.user_action == "quit":
-                continue
-            else: 
-                print("Not a valid entry.")
-
-    def swiped_left(self): # activate cascade of methods constituting a swipe to the left
-        self.move_left()
-        self.combine_left()
-        self.move_left()
-        self.add_block()
-        if self.game_status != "Game Over":
-            print(self.positions)
-
-    def combine_left(self): # combine like blocks, starting from the left
-        for idx, i in enumerate(self.positions): 
-            if idx != len(self.positions)-1:
-                if self.positions[idx] == self.positions[idx+1]:
-                    self.positions[idx] = i*2
-                    self.positions[idx+1] = 0
-    
-    def move_left(self): # move blocks left
-        self.target_idx = -1
-        for idx,i in enumerate(self.positions): 
-            if i == 0:
-                continue
-            else:
-                for jdx,j in enumerate(self.positions[0:idx]):
-                    if j == 0:
-                        self.target_idx = jdx
-                        break
-                    else: self.target_idx = idx
-                
-                if self.target_idx != idx and self.target_idx != -1:
-                    self.positions[self.target_idx] = self.positions[idx]
-                    self.positions[idx] = 0
-            self.target_idx = -1
-    
-    def add_block(self): # add a new, random block to the board
-        empty_idx = []
-        for idx,i in enumerate(self.positions):
-            if i==0:
-                empty_idx.append(idx)
-        if len(empty_idx) > 0:
-               self.positions[int(random.sample(empty_idx,1)[0])] = 1
-        else: 
-            print("Game Over")
-            self.game_status = "Game Over"
-
 class board:
     def __init__(self,size):
         self.positions = np.zeros((size,size))
@@ -110,28 +47,25 @@ class board:
 
     def move(self): # move blocks
         self.target_idx = [-1,-1]
+        print(self.tmp_positions)
         for rdx,r in enumerate(self.tmp_positions):         # For a given row...
-            #print(rdx,r)
-            for cdx, c in enumerate(r):                     # and a given value in that row...
-                #print(rdx,cdx,int(c))                   
+            for cdx, c in enumerate(r):      
+                print("[",rdx,",",cdx,"]",c)               # and a given value in that row...                  
                 if c == 0:                                  # if that value equals 0, do nothing and look at the next                                                  value.
                     continue
                 else:                                       # If that value didn't equal 0...
-                    for jdx,j in enumerate(r[0:cdx]):       # look along the row again...
+                    for jdx,j in enumerate(r[:cdx]):       # look along the row again...
                         if j == 0:                          # and if a value is equal to zero...
                             self.target_idx = [rdx,jdx]     # remember the index of that value...
                             break                           # then get out of this loop.
-                        else: self.target_idx = [rdx,cdx]   # If we don't find any 0, it means the value in question is already butted up against another value, so remember its current location.
-                    
-                if self.target_idx != [rdx,cdx] and self.target_idx != [-1,-1]: # If the value in question needs to move, and if our target index value is valid...
-                    print(self.tmp_positions[self.target_idx])
-                    print(self.tmp_positions[rdx,cdx])
-                    print(self.target_idx,[rdx,cdx],r)
-                    self.tmp_positions[self.target_idx] = self.tmp_positions[rdx,cdx]
-                    print(self.tmp_positions[self.target_idx])
-                    self.tmp_positions[rdx,cdx] = 0
-                    print(self.target_idx,[rdx,cdx],r)
-                self.target_idx = [-1,-1]
+                        else:
+                            self.target_idx = [rdx,cdx]   # If we don't find any 0, it means the value in question is already butted up against another value, so remember its current location.
+                    if self.target_idx != [rdx,cdx] and self.target_idx != [-1,-1]: # If the value in question needs to move, and if our target index value is valid...
+                        print(self.target_idx)
+                        print(self.tmp_positions[self.target_idx])
+                        self.tmp_positions[self.target_idx] = self.tmp_positions[rdx,cdx]
+                        self.tmp_positions[rdx,cdx] = 0
+                    self.target_idx = [-1,-1]
 
     def combine(self): # combine like blocks, starting from the left
         for rdx,r in enumerate(self.tmp_positions):
