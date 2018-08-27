458c99e322503156d85ecf64464f79634abeedc6
Daniel Kaschel <dipique@gmail.com>
2018-08-27 10:35:27 -0500

    Autoplay
    Made the autoplay functionality a little easier to use

--- AutoPlay_Discrete.py
+++ AutoPlay_Discrete.py
@@ -2,6 +2,9 @@ from BoardDisplay import BoardDisplay
 from BoardState import BoardState
 from Direction import Direction
 import random
+import numpy
+
+choices = [Direction.Down, Direction.Up, Direction.Left, Direction.Right]
 
 class AutoPlayMethods:
     @staticmethod
@@ -9,15 +12,33 @@ class AutoPlayMethods:
         step_number = 0
         while not board_state.Lost:
             step_number += 1
-            print(step_number)
-            print(board_state.get_2d_state())
+            # print(step_number)
+            # print(board_state.get_2d_state())
             board_state.move(random.choice(choices))
+        return step_number # returns number of moves
 
-choices = [Direction.Down, Direction.Up, Direction.Left, Direction.Right]
-board_state = BoardState()
+####################### SET TEST PARAMETERS HERE #######################
+save_to_file = True
+output_filename = "output.txt" # set output filename
+number_of_plays = 20 # number of iterations to test autoplay method
+autoplay_method = "random_play" # pick the method to run here
+########################################################################
 
-# pick the method to run here
-AutoPlayMethods.random_play()
+# prepare for run
+results = []
+method_to_call = getattr(AutoPlayMethods, autoplay_method)
 
-board = BoardDisplay(board_state)
-print("Score: " + str(board_state.get_score()))
+# perform test iterations
+for x in range(number_of_plays):
+    board_state = BoardState() #initialize new board
+    steps = method_to_call()
+    result = [x + 1, steps, board_state.get_score()]
+    results.append(result)
+    print(result) # prints results
+
+# save results to text file
+if save_to_file:
+    numpy.savetxt(output_filename, results, '%i', '\t', header="Iteration\tSteps\tScore")
+
+# board = BoardDisplay(board_state) this displays the state of the board in the GUI
+# print("Score: " + str(board_state.get_score()))
