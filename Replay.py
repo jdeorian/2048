50b8f9d119cf4aa6f0cf58ab1889249412383eef
Daniel Kaschel <dipique@gmail.com>
2018-10-08 11:10:41 -0500

    Moved jd files into their own directory since they're no longer compatible with the board state

--- Replay.py
+++ Replay.py
@@ -3,8 +3,8 @@ from game.BoardState import BoardState
 from game.Move import Move
 import numpy as np
 
-# this file is here just for testing, but usually you'd put in a filename from the log folder
-file_to_import = 'logs/random_20180910_090903.114421.log' # 'logs/random_20180906_095102.239753.log'
+# put in a filename from the log folder
+file_to_import = 'logs/branch_20181005_140715.392990.log' # 'logs/random_20180906_095102.239753.log'
 
 log_file = open(file_to_import, "r")
 moves = list(map(Move.from_log_entry, log_file.readlines()))
