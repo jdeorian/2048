83f8befbddff931f7dd7b749bb5f0c89ae4e0794
Daniel Kaschel <dipique@gmail.com>
2018-10-12 10:27:09 -0500

    Pulled some code out of program.cs into their own classes

---
+++ cs/2048_c_sharp/DB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+
+namespace _2048_c_sharp
+{
+    public class DB
+    {
+        private SQLiteConnection conn;
+
+        public DB(string filename)
+        {
+            bool firstTime = !File.Exists(filename);
+            if (firstTime) SQLiteConnection.CreateFile(filename);
+
+            conn = new SQLiteConnection($"Data Source={filename};Version=3;");
+            conn.Open();
+
+
+
+        }
+
+        private void InitializeTables()
+        {
+
+        }
+
+        public void Close()
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ cs/2048_c_sharp/PerfTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048_c_sharp
+{
+    public static class PerfTest
+    {
+        static void Run()
+        {
+            int tries = 100000;
+
+            var squares = new int[4, 4];
+            squares[2, 1] = 2;
+            squares[3, 0] = 1;
+            var start = DateTime.Now;
+            for (int x = 0; x < tries; x++)
+            {
+                var es = squares.GetRowWithoutZeros(2);
+                var t = es.Count();
+            }
+            var end = DateTime.Now;
+            var duration = end - start;
+            Console.WriteLine(duration.ToString());
+
+            start = DateTime.Now;
+            for (int x = 0; x < tries; x++)
+            {
+                //var es = squares.EnumerateOutcomes3(new Move(Direction.Up), .11f);
+                //var t = es.Count();
+            }
+            end = DateTime.Now;
+            duration = end - start;
+            Console.WriteLine(duration.ToString());
+
+            start = DateTime.Now;
+            for (int x = 0; x < tries; x++)
+            {
+                var es = squares.GetRowWithoutZeros(2);
+                var t = es.Count();
+            }
+            end = DateTime.Now;
+            duration = end - start;
+            Console.WriteLine(duration.ToString());
+        }
+    }
+}
