0ef248efef2e67390a6dd583a00629cadb829a92
Daniel Kaschel <dipique@gmail.com>
2018-10-25 15:49:53 -0500

    A cool little extension to allow tuple looping dictionaries. Including even though I'm not using it yet.

---
+++ cs/2048_c_sharp/Utilities/KVPExtentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048_c_sharp.Utilities
+{
+    public static class KVPExtentions
+    {
+        public static void Deconstruct<TKey, TValue>(
+                    this KeyValuePair<TKey, TValue> entry,
+                    out TKey key,
+                    out TValue value)
+        {
+            key = entry.Key;
+            value = entry.Value;
+        }
+    }
+}
