4150e25b474fccb900267a032e325fd6d116eeae
Daniel Kaschel <dipique@gmail.com>
2018-10-25 09:54:43 -0500

    Moved training updates into the conductor class where they belong

--- cs/2048_c_sharp/Auto/Base/ConductorBase.cs
+++ cs/2048_c_sharp/Auto/Base/ConductorBase.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data.SQLite;
+
 using static _2048_c_sharp.Auto.Logger;
 
 namespace _2048_c_sharp.Auto
 {
-    public abstract class ConductorBase<T> where T: AutoBase
+    public abstract class ConductorBase<T> where T : AutoBase
     {
         static readonly string EX_MSG = $"Press ESC to stop.";
         public Board BestBoard { get; set; }
@@ -16,8 +18,32 @@ namespace _2048_c_sharp.Auto
         const int MAX_CONCURRENT_BOARDS = 8;
         const int SLEEP_LENGTH = 100; //in ms
 
+        private SQLiteConnection _conn = null;
+        private SQLiteConnection conn
+        {
+            get
+            {
+                if (_conn == null)
+                {
+                    _conn = new SQLiteConnection(RLTrainingSettings.ConnectionString);
+                    _conn.Open(); //automatically creates file if it doesn't exist
+                    InitializeDB(_conn); //creates tables if they don't exist
+                }                
+                return _conn;
+            }
+        }
+
+        private RLTrainingSettings _settings = null;
+        public RLTrainingSettings RLTrainingSettings => _settings ?? (_settings = GetTrainingSettings());
+
+        /////////// abstract methods //////////////
+        public abstract RLTrainingSettings GetTrainingSettings();
+        public abstract string GetInsertRecordSQL(Move move, float finalScore);
+        ///////////////////////////////////////////
+
         public void Run(bool async = false)
         {
+
             int it_cnt = 1;
             tasks = Enumerable.Range(0, MAX_CONCURRENT_BOARDS)
                               .Select(i => Task.Run(() => { }))
@@ -69,5 +95,44 @@ namespace _2048_c_sharp.Auto
             BestBoard = board.Board;
 
         }
+
+        #region Update Training DB
+
+        private void InitializeDB(SQLiteConnection connection)
+        {
+            //create table
+            string sql = RLTrainingSettings.DBInitializationSQL; //"CREATE TABLE IF NOT EXISTS training_raw (Id INTEGER, Decision VARCHAR(4), Score REAL)";
+            var command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+        }
+
+        private void InsertRawTrainingRecord(Move move, float finalScore)
+        {
+            var sql = GetInsertRecordSQL(move, finalScore); // $"INSERT INTO training_raw (Id, Decision, Score) values ({id}, '{decision}', {score})";
+            var command = new SQLiteCommand(sql, conn);
+            command.ExecuteNonQuery();
+        }
+
+        private void InsertBoardMoveHistoryRawTrainingRecords(Board board)
+        {
+            var tr = conn.BeginTransaction();
+            foreach (var m in board.MoveHistory)
+            {
+                var command = conn.CreateCommand();
+                command.CommandText = GetInsertRecordSQL(m, board.Score); // $"INSERT INTO training_raw (Id, Decision, Score) values ({m.StartState.CanonicalFieldID()}, '{m.Direction}', {board.Score})";
+                command.ExecuteNonQuery();
+            }
+
+            //commit
+            try { tr.Commit(); }
+            catch (Exception)
+            {
+                Console.WriteLine("Error writing training data, rolled back");
+                tr.Rollback();
+            }
+            finally { tr.Dispose(); }
+        }
+
+        #endregion
     }
 }
---
+++ cs/2048_c_sharp/Auto/RandomPlay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2048_c_sharp.Auto
+{
+    public class RandomPlay: AutoBase
+    {
+        public RandomPlay(int i) : base(i) { }
+        public override Dictionary<Direction, float> GetMoveWeights() => XT.EnumVals<Direction>().ToDictionary(k => k, k => 0f);
+    }
+}
